8093face93fa202cf484cf35eadc49995806a1d4
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-30 13:02:45 +0300

    gro

--- js/script.js
+++ js/script.js
@@ -2,5 +2,36 @@ $("document").ready(function(){
     $("body").append("<h1>hello its kim</h1>");
     $("h1").css("color", "blue");
 
+    $("#show").on("click", function(){
+        $("#para").show();
+    });
+    $("#hide").on("click", function(){
+        $("#para").hide();
+    });
     
+
+    $("#see").on("click", function(){
+        $("#textbox").show();
+    })
+    $("#nosee").on("click", function(){
+        $("#textbox").hide();
+    })
+    $("#fade-in").on("click", function(){
+        $("#togo").fadeIn("fast")
+    })
+    $("#slide").on("click", function(){
+        $("img").slideUp("2000","linear")
+    })
+    $("#slideDown").on("click", function(){
+        $("img").slideDown("2000","linear")
+    })
+    $("#toggle").on("click", function(){
+        $("img").toggle("2000","linear")
+    })
+    $("#grow").on("click", function(){
+        $("#img2").animate({width: "500px"})
+    })
+    $("#grow1").on("click", function(){
+        $("#img2").animate({width: "200px"})
+    })
 });
